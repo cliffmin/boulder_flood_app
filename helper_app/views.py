2d1adfac2375c6de6a1fb90e1df232595201cbdb
Cliff <cliffmin@gmail.com>
2014-03-03 18:13:16 -0700

    changed file names

--- helper_app/views.py
+++ helper_app/views.py
@@ -5,14 +5,13 @@ from flask.views import MethodView
 from flask.ext.mongoengine.wtf import model_form
 from app.models import *
 
-dinosaurs = Blueprint('dinosaurs', __name__, template_folder='templates')
+helpers = Blueprint('helpers', __name__, template_folder='templates')
 
 class Search(MethodView):
 
   def get(self):
-    name = request.args.get('name', '')
-
-    dinosaur_type = request.args.get('type', '')
+    location = request.args.get('location', '')
+    skill = request.args.get('skill', '')
 
     try:
       pets = Pets.objects.filter(name__contains=name, pet_type__contains=pet_type)
@@ -30,22 +29,22 @@ class Home(MethodView):
 
 
   def get(self):
-    dinosaurs = Dinosaurs.objects.all()
-    return render_template('dinosaurs/home.html', dinosaurs=dinosaurs)
+    helpers = helpers.objects.all()
+    return render_template('helpers/home.html', helpers=helpers)
 
 class ShowDinosaur(MethodView):
 
   def get(self, slug):
     print slug
-    dinosaur = Dinosaurs.objects.get_or_404(slug=slug)
-  # dinosaur  = Dinosaurs(name="t-rex", height="70m", dinosaur_type="badass", imageURL="http://1.bp.blogspot.com/-u-rpqjoaTI8/UjndjDF44OI/AAAAAAAAA4o/ty_qWLXlCuo/s1600/Dinosaurs_Lasers.jpg)")
-    return render_template('dinosaurs/show.html', dinosaur=dinosaur)
+    dinosaur = helpers.objects.get_or_404(slug=slug)
+  # dinosaur  = helpers(name="t-rex", height="70m", dinosaur_type="badass", imageURL="http://1.bp.blogspot.com/-u-rpqjoaTI8/UjndjDF44OI/AAAAAAAAA4o/ty_qWLXlCuo/s1600/helpers_Lasers.jpg)")
+    return render_template('helpers/show.html', dinosaur=dinosaur)
 
 
-class CreateDinosaur(MethodView):
+class CreateHelper(MethodView):
 
   def get(self):
-    return render_template('dinosaurs/create.html')
+    return render_template('helpers/create.html')
 
   def post(self):
 
@@ -55,16 +54,16 @@ class CreateDinosaur(MethodView):
       height = request.form['height']
       image = request.form['image']
       slug = slugify(name)
-      dinosaur = Dinosaurs(name=name, dinosaur_type=dinosaur_type, height=height, imageURL=image, slug=slug)
+      dinosaur = helpers(name=name, dinosaur_type=dinosaur_type, height=height, imageURL=image, slug=slug)
       dinosaur.save()
 
-      return render_template('dinosaurs/show.html', dinosaur=dinosaur)
-    return render_template('dinosaurs/create.html')
+      return render_template('helpers/show.html', dinosaur=dinosaur)
+    return render_template('helpers/create.html')
 
 
-dinosaurs.add_url_rule('/', view_func=Home.as_view('home'))
-dinosaurs.add_url_rule('/search', view_func=Search.as_view('search'))
-dinosaurs.add_url_rule('/create', view_func=CreateDinosaur.as_view('create'))
-dinosaurs.add_url_rule('/dinosaurs/<slug>/', view_func=ShowDinosaur.as_view('show'))
+helpers.add_url_rule('/', view_func=Home.as_view('home'))
+helpers.add_url_rule('/search', view_func=Search.as_view('search'))
+helpers.add_url_rule('/create', view_func=CreateDinosaur.as_view('create'))
+helpers.add_url_rule('/helpers/<slug>/', view_func=ShowDinosaur.as_view('show'))
 
 
